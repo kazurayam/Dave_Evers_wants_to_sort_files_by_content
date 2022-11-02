9236ac1e6d79159821f2a54ebf64ee452fa0e286
kazurayam <kazuaki.urayama@gmail.com>
2022-11-03 07:05:09 +0900

    use SortingFilesByDateTimeInContent-0.3.2.jar

--- Scripts/TC/Script1667133570111.groovy
+++ Scripts/TC/Script1667133570111.groovy
@@ -6,26 +6,31 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.stream.Collectors
+import java.util.Comparator
 
 import com.kms.katalon.core.configuration.RunConfiguration
 
-import com.kazurayam.study20221030.PathComparableByDateTime
-import com.kazurayam.study20221030.PathComparableByContentEmailDate
+import com.kazurayam.study20221030.IPathComparable
+import com.kazurayam.study20221030.PathComparableByFileLastModified
+import com.kazurayam.study20221030.PathComparableByContentEmailHeaderValue
 
 Path dataDir = Paths.get(RunConfiguration.getProjectDir()).resolve("Include/data")
 
-List<PathComparableByDateTime> emlFiles =
+List<IPathComparable> files =
 	Files.list(dataDir)
-		.filter({ p -> p.toString().endsWith(".eml") })
-		// wrap the Path object by a adapter class
-		// to sort the Path objects by the Email Date in the file content
-		.map({ p -> new PathComparableByContentEmailDate(p) })
-		// in the descending order of the Date value
+		.filter({ p -> p.getFileName().toString().endsWith(".eml") })
+		// filter files with "To: xyz.com" header
+		.map({ p -> new PathComparableByContentEmailHeaderValue(p, "To") })
+		.filter({ p -> p.getValue().matches("xyz.com") })
+		// to sort by the lastModified timestamp
+		.map({ p -> new PathComparableByFileLastModified(p.get()) })
+		// sort in descending order
 		.sorted(Comparator.reverseOrder())
+		// turn the Stream into a List
 		.collect(Collectors.toList())
 		
 // print path of all eml files
-emlFiles.eachWithIndex  { p, index ->
-	println((index + 1) + "\t" + p.getTimestampFormatted() + "\t" + dataDir.relativize(p.get()))
+files.eachWithIndex  { p, index ->
+	println((index + 1) + "\t" + p.getValue() + "\t" + dataDir.relativize(p.get()))
 }
 
